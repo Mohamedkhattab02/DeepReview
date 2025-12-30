3f4ea85defb939591465b22acf21c5ccf54f451b
joyride200s <khaled.habashy2002@gmail.com>
2025-12-30 12:03:02 +0200

    Update supabase.ts

--- my-supabase-app/src/lib/supabase.ts
+++ my-supabase-app/src/lib/supabase.ts
@@ -1,4 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
+import { Timestamp } from "next/dist/server/lib/cache-handlers/types";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
